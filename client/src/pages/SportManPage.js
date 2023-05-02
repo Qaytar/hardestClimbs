e007eb21ff279e82993a64d093fac7c881387b8c
Your Name01~ <you@example.com>
2023-05-02 13:34:10 +0200

    merged all developing functionalities in one page (sportManPage

--- client/src/pages/SportManPage.js
+++ client/src/pages/SportManPage.js
@@ -1,8 +1,15 @@
 import React, { useState } from 'react';
-import { filterSends } from '../utils/functionsHelpers';
+import { filterSends, rankClimbers } from '../utils/functionsHelpers';
 import classes from './SportManPage.module.css';
 
 function SportManPage(props) {
+    //Create a state variavle to display sends chronologicaly or grouped be ranked climbers
+    const [isDisplayData, setDisplayData] = useState('chronological');
+    //Creates a function to toggle the display of the data, it's called when the button is clicked
+    const toggleDisplayData = () => {
+        setDisplayData(isDisplayData === 'chronological' ? 'byClimber' : 'chronological');
+    };
+
     //Creates a state variable to track whether the grading system is European or American
     const [isGradingSystem, setisGradingSystem] = useState('european');
     //Creates a function to toggle the grading system, it's called when the button is clicked
@@ -24,24 +31,50 @@ function SportManPage(props) {
         }
     };
 
-
+    //calls filteredSend to obtain the sub set off all the sends relevant to this page
+    const filteredSends = filterSends(props.data, 'sport', 'man')
+    //calls rankClimbers to obtain the ranking of the climbers included in the filteredSends
+    const rankedClimbers = rankClimbers(filteredSends);
+    console.log('rankedClimbers', rankedClimbers)
     return (
         <div>
             <button onClick={toggleGradeDisplay}>
                 {isGradingSystem === 'american' ? 'Change to European Grades' : 'Change to American Grades'}
             </button>
+            <button onClick={toggleDisplayData}>
+                {isDisplayData === 'chronological' ? 'Change to grouped by climber' : 'Change to chronological order'}
+            </button>
             <h1>SportManPage</h1>
-            {filterSends(props.data, 'sport', 'man').map((send, index) => (
-                //this <p> needs to be a component <Send /> but I couldnt make it work because that file uses so many things: handleMouseEnter, handleMouseLeave,toggleGradeDisplay, etc.
-                <p key={index}>
-                    {send.route.name}
-                    (<span className={classes.grade} onMouseEnter={handleMouseEnter} onMouseLeave={handleMouseLeave}>
-                        {isGradingSystem === 'american' ? send.route.americanGrade : send.route.europeanGrade}
-                        {send.route.note && (<span className={classes.gradeNotePopup}>{send.route.note}</span>)}
-                    </span>)
-                    , by {send.climber.name} on {send.date}
-                </p>
-            ))}
+            {
+                isDisplayData === 'chronological' ? (
+                    filteredSends.map((send, index) => (
+                        <p key={index}>
+                            {send.route.name}
+                            (<span className={classes.grade} onMouseEnter={handleMouseEnter} onMouseLeave={handleMouseLeave}>
+                                {isGradingSystem === 'american' ? send.route.americanGrade : send.route.europeanGrade}
+                                {send.route.note && (<span className={classes.gradeNotePopup}>{send.route.note}</span>)}
+                            </span>)
+                            , by {send.climber.name} on {send.date}
+                        </p>
+                    ))
+                ) : (
+                    rankedClimbers.map((rankedClimber, index) => (
+                        <p key={index}>
+                            {rankedClimber.climber.name} - {
+                                rankedClimber.sends.sport.map((route, index) => (
+                                    <span key={index}>
+                                        {route.name}
+                                        <span className={classes.grade} onMouseEnter={handleMouseEnter} onMouseLeave={handleMouseLeave}>
+                                            ({isGradingSystem === 'american' ? route.americanGrade : route.europeanGrade})
+                                            {route.note && (<span className={classes.gradeNotePopup}>{route.note}</span>)}
+                                        </span>
+                                    </span>))
+                            }
+                        </p>
+                    ))
+                )
+            }
+
         </div>
     );
 }
--- client/src/pages/SportWomanPage2.js
+++ client/src/pages/SportWomanPage2.js
@@ -6,21 +6,9 @@ function SportWomanPage2(props) {
     //calls filteredSend to obtain the sub set off all the sends relevant to this page
     const filteredSends = filterSends(props.data, 'sport', 'woman')
     //calls rankClimbers to obtain the ranking of the climbers included in the filteredSends
-    //Attention! this will return an array of 'send.climber' since it only has access to an array of the model Send
+    //Attention! ChatGPT coded rankClimbers in a way that returns an array of objects with Climbers on it and then Sends. Within Sends, u got Sports and Boulders and then the details. I don't like it, I 'd rather put all send-details straight but will leave it like this for now.
     const rankedClimbers = rankClimbers(filteredSends);
     console.log('rankedClimbers', rankedClimbers)
-
-    // //creates an array of the names of the climbers included in the rankedClimbers to be able to filter the sends by climber name
-    // const rankedClimbersNames = rankedClimbers.map((rankedClimber) => (
-    //     rankedClimber.climber.name
-    // ))
-
-    // //uses such array to filter the sends climbed in the ranked list of climber
-    // //This may seem unnecessary but rankClimbers() returns an array of objects of the model Send, in other words an array of send.climber so I can't access send.climber.send.route.name
-    // //That's the reason I am using the return of rankClimbers() just as a reference to filter the sends in the filteredSends array. Tho it's also used to render the climber in the right order
-    // const rankedClimbersSends = filteredSends.filter((send) => (
-    //     rankedClimbersNames.includes(send.climber.name)
-    // ))
 
     return (
         < div >
@@ -28,14 +16,8 @@ function SportWomanPage2(props) {
             {rankedClimbers.map((rankedClimber, index) => (
                 <p key={index}>
                     {rankedClimber.climber.name} - {
-                        rankedClimber.sends.sport.map((route, index) => (<span key={index}>{route.name} ({route.grade})</span>))
+                        rankedClimber.sends.sport.map((route, index) => (<span key={index}>{route.name} ({route.europeanGrade})</span>))
                     }
-
-                    {/* {rankedClimbersSends.map((send, index) => (
-                        send.climber.name === rankedClimber.climber.name ? (<span key={index}>
-                            {send.route.name} ({send.route.europeanGrade}, {send.date})
-                        </span>) : null
-                    ))} */}
                 </p>
             ))
             }
